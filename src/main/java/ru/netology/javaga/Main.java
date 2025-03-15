e246d51df0d4253acdfb2bb1b4e3e9a654445640
Neto test <veronikakaravanova60@gmail.com>
2025-03-15 17:06:09 +0300

    change README

---
+++ src/main/java/ru/netology/javaga/Main.java
@@ -0,0 +1,12 @@
+package ru.netology.javaga;
+
+import ru.netology.services.monhtsclass;
+
+public class Main {
+    public static void main(String[] args) {
+       monhtsclass service = new monhtsclass();
+
+        int vacations = service.calculate(100000,60000,150000);
+        System.out.println(vacations);
+    }
+}
---
+++ src/main/java/ru/netology/services/monhtsclass.java
@@ -0,0 +1,20 @@
+package ru.netology.services;
+
+public class monhtsclass {
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+        int vacations = 0;
+
+        for (int i = 0; i < 12; i++){
+            if (money >= threshold) {
+                money -= expenses;
+                money = money /3;
+                vacations++;
+            } else {
+                money += income;
+                money -= expenses;
+            }
+        }
+        return vacations;
+    }
+}
